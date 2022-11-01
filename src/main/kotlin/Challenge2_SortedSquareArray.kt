b7ddf45db8f5e85129d68e0984dd6fc156748e6f
otsembo <okumu.otsembo@gmail.com>
2022-11-01 10:39:46 +0300

    Challenge2: Completed

--- src/main/kotlin/Challenge2_SortedSquareArray.kt
+++ src/main/kotlin/Challenge2_SortedSquareArray.kt
@@ -1,6 +1,3 @@
-import java.util.Arrays
-import kotlin.math.pow
-
 // Write a function that takes in a non-empty array of integers that are sorted in ascending order
 // and returns a new array of the same length with the squares of the original integers also sorted in ascending order.
 //For example:
@@ -10,7 +7,7 @@ import kotlin.math.pow
 fun sortedSquareArray(array: Array<Int>): Array<Int>{
     val output = mutableListOf<Int>()
     array.forEach {
-        output.add(it.toDouble().pow(2).toInt())
+        output.add(it*it)
     }
     return output.toTypedArray()
 }
